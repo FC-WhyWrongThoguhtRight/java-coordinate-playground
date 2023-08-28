fff6a548eb29c572cdeb550071e1fb344bc1a39a
jaehyuk <jaehyuk7722@naver.com>
2023-08-28 11:23:35 +0900

    refactor: enum refector 2 complete
    enum refector 2 complete

--- src/main/java/mission2/FigureFactory.java
+++ src/main/java/mission2/FigureFactory.java
@@ -1,30 +1,42 @@
 package mission2;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * https://brad903.tistory.com/entry/enum 블로그 참고
+ * 1. Enum 으로 상수 구현
+ * 2. Map 을 이용한 IF 중첩제거
+ * 3. 람다를 이용한 단순화
  */
-public class FigureFactory implements FigureCreator{
-    //1차 변경
+public class FigureFactory{
+    public static final String ERROR_MESSAGE_VALID = "유효하지 않는 도형입니다.";
+    //중첩 if >> map 을 이용 리팩토링
+    private static Map<Integer, FigureCreator> figures = new HashMap<>();
+
+    //람다를 이용한 static 블록 활용
+    static {
+        figures.put(FigureType.LINE.getNumOfPoint(), points -> new Line(points));
+        figures.put(FigureType.SQUARE.getNumOfPoint(), points -> new Square(points));
+        figures.put(FigureType.TRIANGLE.getNumOfPoint(), points -> new Triangle(points));
+    }
+
+    //enum 을 이용한 1차 리팩토링
     public static Figure getInstance(List<Point> points) {
-        if(FigureType.LINE.matchNumOfPoint(points.size())){
-            return new Line(points);
+        if(points == null){
+            throw new NullPointerException();
         }
 
-        if(FigureType.TRIANGLE.matchNumOfPoint(points.size())){
-            return new Triangle(points);
+        if(isValid(points)){
+            throw  new IllegalArgumentException(ERROR_MESSAGE_VALID);
         }
 
-        if(FigureType.SQUARE.matchNumOfPoint(points.size())){
-            return new Square(points);
-        }
-
-        throw  new IllegalArgumentException("유효하지 않는 도형입니다.");
+        return figures.get(points.size()).create(points);
     }
 
-    @Override
-    public Figure create(List<Point> points) {
-        return null;
+    private static boolean isValid(List<Point> points) {
+        return points.size() < FigureType.LINE.getNumOfPoint() || points.size() > FigureType.SQUARE.getNumOfPoint();
     }
+
 }
